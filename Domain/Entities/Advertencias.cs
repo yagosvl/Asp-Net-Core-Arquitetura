337e0db7886b9aaf31c4cdabe933a09ca88e68d7
yagosvl <yagosvl@hotmail.com>
2018-08-09 11:31:46 -0300

    Primeiro commit criado projeto domínio e suas entidades

---
+++ Domain/Entities/Advertencias.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Advertencias
+    {
+        public int Id { get; set; }
+        public DateTime DataOcorrencia { get; set; }
+        public int EstagiarioId { get; set; }
+
+        public virtual Estagiario Estagi { get; set; }
+    }
+}
---
+++ Domain/Entities/Estagiario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Estagiario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public ICollection<Advertencias> Mancadas { get; set; }
+    }
+}
