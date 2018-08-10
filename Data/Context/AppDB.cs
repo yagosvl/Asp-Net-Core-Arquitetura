09a814d98bc0b39ff4c05a2e238ae70392f9b28e
yagosvl <yagosvl@hotmail.com>
2018-08-10 20:43:33 -0300

    Avançando na configuracao do Contexto

---
+++ Data/Context/AppDB.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Data.Context
+{
+    class AppDB : DbContext
+    {
+        public virtual DbSet<Estagiario> Unit { get; set; }
+        public virtual DbSet<Advertencias> Variable { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new EstagiarioConfig());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the configuration from the app settings
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // define the database to use
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
